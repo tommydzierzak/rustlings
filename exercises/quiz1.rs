fb3392a9ef801066dbf07b1ebf68dad69daed690
Thomas Dzierzak <dzierzakt@gmail.com>
2024-03-12 14:57:07 -0400

    commit to my branch

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(numApp: i32) -> i32 {
+    if numApp <= 40 {
+        numApp*2
+    }
+    else{
+        numApp
+    }
+}
 
 // Don't modify this function!
 #[test]
