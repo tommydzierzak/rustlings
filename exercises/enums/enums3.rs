d24a1e63f70cf3c895363e49f446c8bb211b9fed
Thomas Dzierzak <dzierzakt@gmail.com>
2024-03-12 23:35:51 -0400

    current

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -9,6 +9,10 @@
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit
 }
 
 struct Point {
